2a5cfb9d32c96f9a162b48a4698a986f8ea5b4c1
enco164@gmail.com <enco164@gmail.com@7ce7bccc-32d5-6017-f964-bbce70f79a65>
2014-07-17 08:20:08 +0000

    ADD DeviceHive Lib

---
+++ trunk/DeviceHive-ClientAndroid/src/com/android/devicehive/device/DeviceStatusNotification.java
@@ -0,0 +1,42 @@
+package com.android.devicehive.device;
+
+import java.util.HashMap;
+
+import com.android.devicehive.DeviceData;
+import com.android.devicehive.Notification;
+
+/**
+ * Represents a device status notification which is sent by {@link Device} when
+ * its status changes.
+ */
+public class DeviceStatusNotification extends Notification {
+
+	/**
+	 * Predefined "Online" device status notification.
+	 */
+	public static final DeviceStatusNotification STATUS_ONLINE = new DeviceStatusNotification(
+			DeviceData.DEVICE_STATUS_ONLINE);
+
+	/**
+	 * Predefined "OK" device status notification.
+	 */
+	public static final DeviceStatusNotification STATUS_OK = new DeviceStatusNotification(
+			DeviceData.DEVICE_STATUS_OK);
+
+	/**
+	 * Construct device status notification with given device status.
+	 * 
+	 * @param deviceStatus
+	 *            Device status value.
+	 */
+	public DeviceStatusNotification(String deviceStatus) {
+		super("DeviceStatus", parametersWithDeviceStatus(deviceStatus));
+	}
+
+	private static HashMap<String, Object> parametersWithDeviceStatus(
+			String deviceStatus) {
+		HashMap<String, Object> parameters = new HashMap<String, Object>();
+		parameters.put("status", deviceStatus);
+		return parameters;
+	}
+}
